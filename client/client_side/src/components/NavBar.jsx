e2952e25e5132c10a9f10f0d52555a1972dbc633
aziz <abdulazizjambi98@gmail.com>
2024-08-08 12:42:27 +0700

    add cors

--- client/client_side/src/components/NavBar.jsx
+++ client/client_side/src/components/NavBar.jsx
@@ -2,33 +2,33 @@ import React, { useState } from "react";
 
 export default function NavBar() {
   return (
-    <div className=" w-screen relative">
-      <nav className="relative border-0 border-gray-600 ">
-        <div className="container mx-auto flex justify-  items-center px-4 py-3 bg-white  bg-opacity-50  fixed">
-          <div className="flex w-1/3 ">
-            <span className="text-xl font-semibold text-gray-800 ml-20  ">
-              Tasteful
-            </span>
-          </div>
-          <div className="w-1/3"></div>
-          <div className="flex space-x-4 w-1/3 justify-center  ">
-            <a href="#home" className="text-gray-800 hover:text-blue-500">
-              About
-            </a>
-            <a href="#about" className="text-gray-800 hover:text-blue-500">
-              Ai
-            </a>
-            <a href="#recipes" className="text-gray-800 hover:text-blue-500">
-              Recipes
-            </a>
-
-            <a href="#home" className="text-gray-800 hover:text-blue-500">
-              Login
-            </a>
-          </div>
+    <div className="relative w-screen">
+    <nav className="relative border-0  w-screen">
+      <div className="w-screen container mx-auto flex justify-between items-center px-4 py-3 bg-black bg-opacity-50 fixed">
+        <div className="flex w-1/3">
+          <span className="text-xl font-semibold text-white ml-20">
+            Tasteful
+          </span>
         </div>
-      </nav>
-    </div>
+        <div className="w-1/3"></div>
+        <div className="flex space-x-4 w-1/3 justify-center text-white">
+          <a href="#home" className=" hover:text-blue-500">
+            About
+          </a>
+          <a href="#about" className=" hover:text-blue-500">
+            Ai
+          </a>
+          <a href="#recipes" className=" hover:text-blue-500">
+            Recipes
+          </a>
+          <button href="#home" className="text-black flex font-bold justify-center items-center hover:bg-black hover:text-white  bg-white w-14 h-7  rounded-md">
+            Login
+          </button>
+        </div>
+      </div>
+    </nav>
+  </div>
+  
     // <div>Tasteful</div>
   );
 }
